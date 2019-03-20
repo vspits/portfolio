23c960a546a76bb1ea1e80e4e85f4869944d1961
vivian <vivian@vivians-MacBook-Pro.local>
2019-03-20 11:16:01 -0600

    filled out

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,21 @@ import PropTypes from 'prop-types'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            {/* <span className="icon fa-diamond"></span> */}
+            <img src={require('./../images/floweroflifewhite.png')} className='img-flower'/> 
+            {/* <span className="icon fa-centos"></span> */}
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Vivian Spits</h1>
-                <p>Junior Web Developer</p>
+                <p>Web Developer</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Skills</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Projects</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import Headshot from '../images/Headshot.jpg'
+import GardenSpaceWalkThrough4K from '../images/GardenSpaceWalkThrough4K.mp4'
+// import GSShopDashboard from '../images/GSShopDashboard.png'
 
 class Main extends React.Component {
   render() {
@@ -14,31 +15,54 @@ class Main extends React.Component {
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <h2 className="major">About</h2>
+          <p>After some time of indecision about what to do with my career path, I stumbled upon the tech industry and inevitably fell into the rabbit hole of programming. I decided to invest in my future by attending DevMountain where I've learned the foundations of web development and how to approach future learning in the field. </p>
+          <p>Having studied psychology for the past few years, I'm well-versed in human relations and emotional intelligence. I love group dynamics and am a great addition to any team! My hobbies outside the office include dance, park dates with my pup, and eating.</p>
+          <span className="image main"><img src={Headshot} alt=""/></span>
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <h2 className="major">Skills</h2>
+          {/* <span className="image main"><img src={pic02} alt="" /></span> */}
+          <p style={{letterSpacing: '2px', textAlign: 'center'}} className='skillset'>REACT.JS&ensp;|&ensp;NODE.JS&ensp;|&ensp;POSTGRESQL&ensp;|&ensp;CSS<br/>JAVASCRIPT&ensp;|&ensp;HTML&ensp;|&ensp;REDUX&ensp;|&ensp;SASS&ensp;|&ensp;GIT</p>
           {close}
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <h2 className="major">Projects</h2>
+          <span className="image main">
+            {/* <img src={GSShopDashboard} alt="" /> */}
+            <video width="100%" controls>
+              <source src={GardenSpaceWalkThrough4K} type='video/mp4'/>
+            </video>
+          </span>
+          <p>
+            <i><p style={{fontSize: '28px'}}>GardenSpace:</p></i>
+            <p style={{fontSize: '14px'}}>React.js | Node.js | PostgreSQL | Bootstrap | Redux | HTML5 | CSS3 | Axios | Express | Bcryptjs</p>
+            A personally developed eCommerce web application where users can view categories, products, and interact with a shopping cart to purchase products. Media queries are used to optimize the mobile experience for a user. Authentication is included utilizing bcrypt for information security.
+          </p>
+          <p>Live site:&emsp;<i><a href='https://gardenspace.xyz/#/shop/category' target="_blank" rel="noopener noreferrer">gardenspace.xyz</a></i><br/>GitHub Repo:&emsp;<i><a href='https://github.com/vspits/personal-project' target='_blank' rel="noopener noreferrer">https://github.com/vspits/personal-project</a></i></p>
           {close}
         </article>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <ul className="icons">
+            <li><a href="https://www.linkedin.com/in/vivianspits/" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer"><span className="label">LinkedIn</span></a></li>
+            <span><a href="https://www.linkedin.com/in/vivianspits/" target="_blank" rel="noopener noreferrer">linkedin.com/vivianspits</a></span>
+            <br/>
+            <br/>
+            {/* <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li> */}
+            <li><a href="https://github.com/vspits" className="icon fa-github" target="_blank" rel="noopener noreferrer"><span className="label">GitHub</span></a></li>
+            <span><a href="https://github.com/vspits" target="_blank" rel="noopener noreferrer">github.com/vspits</a></span>
+            <br/>
+            <br/>
+            <li><a href="mailto:spits.vivian@gmail.com?Subject=Portfolio%20Inquiry" className='icon fa-envelope' target='_blank' rel='noopener noreferrer'><span className='label'>Email</span></a></li>
+            <span><a ref="mailto:spits.vivian@gmail.com?Subject=Portfolio%20Inquiry" target='_blank' rel='noopener noreferrer'>spits.vivian@gmail.com</a></span>
+          </ul>
+          {/* <form method="post" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
@@ -55,13 +79,7 @@ class Main extends React.Component {
               <li><input type="submit" value="Send Message" className="special" /></li>
               <li><input type="reset" value="Reset" /></li>
             </ul>
-          </form>
-          <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
-          </ul>
+          </form> */}
           {close}
         </article>
 
