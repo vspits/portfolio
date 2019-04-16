81c09362fd88d606a4b035e81a067de2f071f727
vspits <spits.vivian@gmail.com>
2019-04-15 18:35:05 -0600

    added classcast

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
+// // // // // IMPORTS // // // // //
+
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import pic02 from '../images/pic02.jpg'
 import Headshot from '../images/Headshot.jpg'
-import GardenSpaceWalkThrough4K from '../images/GardenSpaceWalkThrough4K.mp4'
-// import GSShopDashboard from '../images/GSShopDashboard.png'
+import GardenSpaceWalkThrough4K from '../images/GardenSpaceWalkThrough4K.mp4';
+import classcastwalkthrough from '../images/classcastwalkthrough.mp4'
 
 class Main extends React.Component {
   render() {
@@ -24,25 +24,39 @@ class Main extends React.Component {
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Skills</h2>
-          {/* <span className="image main"><img src={pic02} alt="" /></span> */}
           <p style={{letterSpacing: '2px', textAlign: 'center', fontSize: '22px'}} className='skillset'>REACT.JS&ensp;|&ensp;NODE.JS&ensp;|&ensp;POSTGRESQL&ensp;|&ensp;CSS<br/>JAVASCRIPT&ensp;|&ensp;HTML&ensp;|&ensp;REDUX&ensp;|&ensp;SASS&ensp;|&ensp;GIT</p>
           {close}
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Projects</h2>
+
+            <i><p style={{fontSize: '28px'}}>ClassCast:</p></i>
           <span className="image main">
-            {/* <img src={GSShopDashboard} alt="" /> */}
+            <video width="100%" controls>
+              <source src={classcastwalkthrough} type='video/mp4'/>
+            </video>
+          </span>
+          <p>
+            <p style={{fontSize: '14px'}}>React.js | Node.js | PostgreSQL | Socket.IO | WebRTC | Redux | HTML5 | CSS3 | Axios | Express | Bcryptjs</p>
+            <i>April 2019:&ensp;</i>A group-developed application which allows teachers to live stream a lecture for students, and offers students the ability to interact and ask questions during the lecture. WebRTC and Socket.IO are utilized for the functionality of the site.
+          </p>
+          <p style={{fontSize: '16px'}}>Live site:&emsp;<i><a href='https://classcast.live/' target="_blank" rel="noopener noreferrer">classcast.live</a></i><br/>GitHub Repo:&emsp;<i><a href='https://github.com/online-classroom/classroom' target='_blank' rel="noopener noreferrer">https://github.com/online-classroom/classroom</a></i></p>
+
+          <hr/>
+
+            <i><p style={{fontSize: '28px'}}>GardenSpace:</p></i>
+          <span className="image main">
             <video width="100%" controls>
               <source src={GardenSpaceWalkThrough4K} type='video/mp4'/>
             </video>
           </span>
           <p>
-            <i><p style={{fontSize: '28px'}}>GardenSpace:</p></i>
             <p style={{fontSize: '14px'}}>React.js | Node.js | PostgreSQL | Bootstrap | Redux | HTML5 | CSS3 | Axios | Express | Bcryptjs | Stripe</p>
-            A personally developed eCommerce web application where users can view categories, products, and interact with a shopping cart to purchase products. Media queries are used to optimize the mobile experience for a user. Authentication is included utilizing bcrypt for information security.
+            <i>March 2019:&ensp;</i>A personally developed eCommerce web application where users can view categories, products, and interact with a shopping cart to purchase products. Media queries are used to optimize the mobile experience for a user. Authentication is included utilizing bcrypt for information security.
           </p>
           <p style={{fontSize: '16px'}}>Live site:&emsp;<i><a href='https://gardenspace.xyz/#/shop/category' target="_blank" rel="noopener noreferrer">gardenspace.xyz</a></i><br/>GitHub Repo:&emsp;<i><a href='https://github.com/vspits/personal-project' target='_blank' rel="noopener noreferrer">https://github.com/vspits/personal-project</a></i></p>
+
           {close}
         </article>
 
@@ -53,8 +67,6 @@ class Main extends React.Component {
             <span><a href="https://www.linkedin.com/in/vivianspits/" target="_blank" rel="noopener noreferrer">linkedin.com/vivianspits</a></span>
             <br/>
             <br/>
-            {/* <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li> */}
             <li><a href="https://github.com/vspits" className="icon fa-github" target="_blank" rel="noopener noreferrer"><span className="label">GitHub</span></a></li>
             <span><a href="https://github.com/vspits" target="_blank" rel="noopener noreferrer">github.com/vspits</a></span>
             <br/>
@@ -62,24 +74,6 @@ class Main extends React.Component {
             <li><a href="mailto:spits.vivian@gmail.com?Subject=Portfolio%20Inquiry" className='icon fa-envelope' target='_blank' rel='noopener noreferrer'><span className='label'>Email</span></a></li>
             <span><a ref="mailto:spits.vivian@gmail.com?Subject=Portfolio%20Inquiry" target='_blank' rel='noopener noreferrer'>spits.vivian@gmail.com</a></span>
           </ul>
-          {/* <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form> */}
           {close}
         </article>
 
